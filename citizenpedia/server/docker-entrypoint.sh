0f746ec0544eb0779ba55545ed8bb7c728be7523
mirkoperillo <mirko.perillo@gmail.com>
2018-05-24 16:06:32 +0200

    fix citizenpedia dockerization

--- citizenpedia/server/docker-entrypoint.sh
+++ citizenpedia/server/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp /app-run/config/* /app/server/config/environment
-
+cp /app-run/config/index.js /app/server/config/environment
+cp /app-run/config/production.js /app/server/config/environment
+cp /app-run/config/shared.js /app/server/config/environment
+cp /app-run/config/local.env.js /app/server/config
 grunt serve
